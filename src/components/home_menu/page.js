0289219c9cf9153f681f6e09ddbbdc5c75731adc
Mishan Adhikari <mishanadhikari8@gmail.com>
2024-05-13 21:21:36 +0545

    Initial commit

--- src/components/home_menu/page.js
+++ src/components/home_menu/page.js
@@ -5,12 +5,12 @@ import {Tabs, Tab, Card, CardBody} from "@nextui-org/react";
 const Hmenu = () => {
   return (
     <div className="flex w-fit flex-col ml-2 mt-2">
-      <Tabs aria-label="Options" className=' bg-zinc-950'>
-        <Tab key="photos" title="Photos" className='bg-zinc-950 text-white'>
+      <Tabs aria-label="Options" className=' bg-black'>
+        <Tab key="photos" title="Global" className='bg-black text-white'>
         </Tab>
-        <Tab key="music" title="Music" className='bg-zinc-950 text-white '>
+        <Tab key="music" title="Nepal" className='bg-black text-white '>
         </Tab>
-        <Tab key="videos" title="Videos" className='bg-zinc-950 text-white '>
+        <Tab key="videos" title="following" className='bg-zinc-950 text-white '>
         </Tab>
       </Tabs>
     </div>  
